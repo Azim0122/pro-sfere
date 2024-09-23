aee19f5c0444a05932b5991dd4afd3feacc75cc8
Azim <rokjohn178@gmail.com>
2024-09-24 00:49:36 +0500

    restart

--- src/components/proServices/ProServices.jsx
+++ src/components/proServices/ProServices.jsx
@@ -8,25 +8,53 @@ function ProServices({ category }) {
   return (              
     <div className="boxss">
 
-        {
-          category == "Сервисы" && <div className="boxs">
-
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((item, index) => {
-              return (<div className="box">
-                <div className="box-top">
-                  <span>{index + 1 < 10 ? "0" : ""}{index + 1}</span>
-                  <i class="fa-solid fa-arrow-up"></i>
-                </div>
-                <div className="box-bottom">
-                  <p>PRO <br />Donate</p>
-                </div>
-              </div>)
-            })}
-
-
-
+{
+  category == "Сервисы" && <div className="boxs">
+    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item, index) => {
+      const texts = [
+        "CHEEFF",
+        "GOOD LOOK",
+        "MY TAB",
+        "Influency",
+        "Dozzer",
+        "Share",
+        "Pro id",
+        "Disk",
+        "Crm",
+        "Donate",
+        "Support"
+      ];
+      const links = [
+        "https://cheeff.uz",
+        "https://goodlook.uz",
+        "https://mytab.uz",
+        "http://influency.uz",
+        "http://dozzer.uz",
+        "http://share.uz",
+        "http://id.sfere.pro",
+        "http://disk.sfere.pro",
+        "https://crm.sfere.pro",
+        "http://donate.sfere.pro",
+        "http://support.sfere.pro"
+      ];
+      return (
+        <div className="box" key={index}>
+          <div className="box-top">
+            <span>{index + 1 < 10 ? "0" : ""}{index + 1}</span>
+            <a href={links[index]} target="_blank" rel="noopener noreferrer">
+            <i className="fa-solid fa-arrow-up"></i>
+            </a>
           </div>
-        }
+          <div className="box-bottom">
+           
+              <p>{texts[index]}</p>
+            
+          </div>
+        </div>
+      );
+    })}
+  </div>
+}
         {
           category == "Новости" && <div className="boxs7">
             <div className="btns3">
